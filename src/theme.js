8c28db33821d35e2a5eb4575bb8ae7f82091d145
Ivanov Evhenii <ivanov191192@gmail.com>
2020-12-25 00:49:52 +0200

    fix

--- src/theme.js
+++ src/theme.js
@@ -1,20 +1,24 @@
+const Theme = {
+	LIGHT: "light-theme",
+	DARK: "dark-theme",
+}
 const switchInput = document.querySelector(".theme-switch__toggle")
 const bodyTheme = document.querySelector("body")
 
 const savedTheme = localStorage.getItem("theme")
 if (savedTheme) {
-	bodyTheme.classList.add("dark-theme")
+	bodyTheme.classList.add(Theme.DARK)
 	switchInput.checked = true
 }
 
 switchInput.addEventListener("change", () => {
 	if (switchInput.checked) {
-		bodyTheme.classList.add("dark-theme")
-		bodyTheme.classList.remove("light-theme")
-		localStorage.setItem("theme", "dark-theme")
+		bodyTheme.classList.add(Theme.DARK)
+		bodyTheme.classList.remove(Theme.LIGHT)
+		localStorage.setItem("theme", Theme.DARK)
 	} else {
-		bodyTheme.classList.add("light-theme")
-		bodyTheme.classList.remove("dark-theme")
+		bodyTheme.classList.add(Theme.LIGHT)
+		bodyTheme.classList.remove(Theme.DARK)
 		localStorage.clear()
 	}
 })
